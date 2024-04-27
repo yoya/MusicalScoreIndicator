05ee5323116816d9bd6d444d03c967303aab6757
Yoshihiro Yamazaki <yoya@awm.jp>
2024-04-28 02:04:42 +0900

    楽章をクリックすると、楽章の頭から 小節番号をクリックすると、小節番号の頭から その下をクリックすると、その時間ぴったりから開始。

--- main.js
+++ main.js
@@ -20,14 +20,26 @@ const config = {
               ]
         },
         { comment: "2 movement",
-          bar:[ [83, "5m1s"],
-                
+          bar:[ [83, "5m1s"], [84, "5m31s"], [85, "5m50s"], [86, "6m12s"],
+                [87, "6m25s"],
+                [88, "6m41s"], [89, "6m59s"], [90, "7m11s"], [91, "7m23s"],
                 [92, "7m34s"] ]
         },
         { comment: "3 movement",
-          bar:[ [92, "7m34s"],
-                
-                [131, "11m36s"] ]
+          bar:[ [92, "7m34s"], [93, "7m39s"], [94, "7m44s"],
+                [95, "7m50s"], [96, "7m57s"], [97, "8m3s"],
+                [98, "8m10s"], [99, "8m17s"], [100, "8m23s"],
+                [101, "8m26s"], [102, "8m33s"], [103, "8m40s"],
+                [104, "8m47s"], [105, "8m54s"], [106, "9m1s"], [107, "9m7s"],
+                [108, "9m14s"], [109, "9m20s"], [110, "9m27s"],
+                [111, "9m32s"], [112, "9m40s"],
+                [113, "9m47s"], [114, "9m50s"], [115, "9m58s"],
+                [116, "10m6s"], [117, "10m10s"],
+                [118, "10m17s"], [119, "10m21s"],
+                [120, "10m27s"], [121, "10m32s"],
+                [122, "10m43s"], [123, "10m50s"],[124, "10m54s"], [125,"10m59s"],
+                [126, "11m2s"], [127, "11m9s"], [128, "11m14s"], [129, "11m19s"],
+                [130, "11m26s"], [131, "11m36s"] ]
         },
         { comment: "terminater", bar:[ [131, "11m36s"] ] }
     ]
@@ -66,6 +78,40 @@ function hitVideo(hitTime) {  // progressBar で時間を指示された
     $("#hitTime").innerText = timeToString(hitTime);
     context.hitTime = hitTime;
 }
+function hitProgressBar(x, y, width, height) {
+    const { duration } = context;
+    const t = duration * (x / width);
+    let ret = 0;
+    const choice = (y * 3 / height) | 0;
+    switch (choice) {
+    case 0:
+        for (const t1 of config.timeSchedule) {
+            const tt = stringToTime(t1.bar[0][1]);
+            if (t < tt) {
+                break;
+            }
+            ret = tt;
+        }
+        break;
+    case 1:
+        for (const t1 of config.timeSchedule) {
+            for (const b of t1.bar) {
+                const tt = stringToTime(b[1]);
+                if (t < tt) {
+                    break;
+                }
+                ret = tt;
+            }
+        }
+        break;
+    case 2:
+    default:
+        ret = t;
+        break;
+    }
+    return ret;
+}
+
 
 let baseImageData = null;
 function makeProgressBase() {
@@ -80,31 +126,45 @@ function makeProgressBase() {
         const t2 = stringToTime(t.bar[0][1]);
         if (0 <= prevT) {
             ctx.fillStyle = getNextColor();
-            const x1 = width * prevT / duration;
-            const x2 = width * t2 / duration;
+            const x = width * prevT / duration;
+            const w = width * (t2 - prevT) / duration;
+            const h = height/3;
             ctx.fillStyle = getNextColor();
             ctx.strokeStyle = "gray";
-            ctx.fillRect(x1, 0, x2 - x1, height/3);
+            ctx.fillRect(x, 0, w, h);
             ctx.fillStyle = "black";
-            ctx.fillText(prevComment, x1, height/6);
+            const ty = height / 6;
+            ctx.fillText(prevComment, x, ty);
         }
         prevComment = t.comment;
         prevT = t2;
     }
     for (const t of config.timeSchedule) {
         let prevTT = -1, prevBar;
-        for (const b of t.bar) {
+        for (const i in t.bar) {
+            const b = t.bar[i];
             const [bar, timeStr] = b;
             const tt = stringToTime(timeStr);
             if (0 <= prevTT) {
-                const x1 = width * prevTT / duration;
-                const x2 = width * tt / duration;
+                const x = width * prevTT / duration;
+                const w = width * (tt - prevTT) / duration;
+                const y = height/3;
+                const h = height/3;
                 ctx.fillStyle = getNextColor();
-                ctx.strokeStyle = "gray";
-                console.log({x1, x2, c, prevBar });
-                ctx.fillRect(x1, height/3, x2 - x1, height/3);
+                ctx.fillRect(x, y, w, h);
+            }
+            prevTT = tt;
+            prevBar = bar;
+        }
+        for (const i in t.bar) {
+            const b = t.bar[i];
+            const [bar, timeStr] = b;
+            const tt = stringToTime(timeStr);
+            if (0 <= prevTT) {
+                const x = width * prevTT / duration;
+                const y = [(height*2/5), (height/2), (height*3/5)][((i-1)%3)];
                 ctx.fillStyle = "black";
-                ctx.fillText(prevBar, x1, height/2);
+                ctx.fillText(prevBar, x, y+4);
             }
             prevTT = tt;
             prevBar = bar;
@@ -158,12 +218,10 @@ function main() {
     });
     // botton handler
     $("#progressBarContainer").on(["mousedown", "mousemove", "mouseup"], (e) => {
-        console.log(e.buttons);
         if (! e.buttons) return ;
-        const { width } = $("#progressBar");
-        const { offsetX } = e;
-        const { duration } = context;
-        const t = duration * (offsetX / width)
+        const { width, height } = $("#progressBar");
+        const { offsetX, offsetY } = e;
+        const t = hitProgressBar(offsetX, offsetY, width, height);
         hitVideo(t);
         $("#video").currentTime = t;  // seek video
         setTimeout(() => { playVideo() }, 200);  // 直後に play しても無駄
