f2b3d502801414008583a3984945dc66ebcaa601
Yoshihiro Yamazaki <yoya@awm.jp>
2024-05-29 21:10:34 +0900

    showwave を n を使って横に広げる。(scaleで広げるより綺麗)

--- movie/wave2movie.sh
+++ movie/wave2movie.sh
@@ -10,7 +10,7 @@ r=24
 
 # ref) https://shingoushori.hatenablog.jp/entry/2018/03/04/001806
 
-ffmpeg -y -i $i -filter_complex  \
+ffmpeg -y -i $i -r $r -filter_complex  \
        "[0:a]showspectrum=mode=combined:color=rainbow:scale=cbrt:slide=scroll:s=320x100:fps=$r,pad=320:180[vs]; \
-       [0:a]showwaves=s=160x80:mode=cline:scale=sqrt:colors=yellow,scale=320:80[sw]; \
+       [0:a]aformat=channel_layouts=mono,showwaves=s=640x80:mode=line:scale=sqrt:colors=yellow:n=0.25[sw]; \
         [vs][sw]overlay=0:H-h[out]" -map "[out]" -map 0:a $o
