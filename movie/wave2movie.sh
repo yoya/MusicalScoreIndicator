d852ee70047bffc3aba2286c40f98420e6c510bc
Yoshihiro Yamazaki <yoya@awm.jp>
2024-05-29 16:58:51 +0900

    wave 表示を横方向に倍拡大

--- movie/wave2movie.sh
+++ movie/wave2movie.sh
@@ -6,7 +6,11 @@ set -euo pipefail
 
 i=$1 ; o=$2
 
+r=24
+
+# ref) https://shingoushori.hatenablog.jp/entry/2018/03/04/001806
+
 ffmpeg -y -i $i -filter_complex  \
-       "[0:a]showspectrum=mode=combined:color=rainbow:scale=cbrt:slide=scroll:s=320x100:fps=24,pad=320:180[vs]; \
-       [0:a]showwaves=s=320x80:mode=cline:scale=sqrt[sw]; \
+       "[0:a]showspectrum=mode=combined:color=rainbow:scale=cbrt:slide=scroll:s=320x100:fps=$r,pad=320:180[vs]; \
+       [0:a]showwaves=s=160x80:mode=cline:scale=sqrt:colors=yellow,scale=320:80[sw]; \
         [vs][sw]overlay=0:H-h[out]" -map "[out]" -map 0:a $o
