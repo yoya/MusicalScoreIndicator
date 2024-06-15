163c3dc426c571d7a92ed72f582ad69b5cfe8cad
Yoshihiro Yamazaki <yoya@awm.jp>
2024-06-16 00:43:22 +0900

    作業中の wav は -tmp.wav に改名。.wav だと元がその名前の可能性があって、消えてしまう。

--- movie/movie.sh
+++ movie/movie.sh
@@ -12,8 +12,8 @@ ffmpeg -y -i $b.mp4 -filter_complex "showwavespic=size=1012x40::split_channels=0
 convert $b-wave.png -fx "(p{i,j}+p{i,39-j})*0.6" -crop x20+0 -resize 'x400%!' +repage -crop x40+0+40 $b-wave.png
 zopflipng -y $b-wave.png $b-wave.png
 
-ffmpeg -y -i $b.mp4 $b.wav ;
+ffmpeg -y -i $b.mp4 $b-tmp.wav ;
 
-ffmpeg -y -i $b.wav -filter_complex "showcqt=size=600x180:timeclamp=0.5:rate=$r:gamma=5:gamma2=5:fontcolor=r(1)+g(1)+b(1),crop=448:136:100" $b-spectrum.gif
+ffmpeg -y -i $b-tmp.wav -filter_complex "showcqt=size=600x180:timeclamp=0.5:rate=$r:gamma=5:gamma2=5:fontcolor=r(1)+g(1)+b(1),crop=448:136:100" $b-spectrum.gif
 ffmpeg -y -i $b-spectrum.gif -r $r -pix_fmt yuv420p $b-spectrum.mp4
-rm -f $b.wav $b-spectrum.gif
+rm -f $b-tmp.wav $b-spectrum.gif
